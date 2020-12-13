3774c4caf8be0280c284b380194557a2f74f5190
Oyin Sholeye <oyin.s@wustl.edu>
2020-12-13 16:00:34 -0600

    Done with resources

--- Check-In3.0/CheckInViewController.swift
+++ Check-In3.0/CheckInViewController.swift
@@ -13,6 +13,13 @@ import Firebase
 class CheckInViewController: UIViewController {
     let db = Firestore.firestore()
     var ref: DocumentReference? = nil
+    var sleep = [Resource]()
+    var academics = [Resource]()
+    var depression = [Resource]()
+    var stress = [Resource]()
+    var anxiety = [Resource]()
+    var general = [Resource]()
+    var double = [[Resource]]()
     
     @IBOutlet weak var sleepBtn: UIButton!
     
@@ -24,6 +31,7 @@ class CheckInViewController: UIViewController {
     @IBOutlet weak var anxietyBtn: UIButton!
     
    
+    @IBOutlet weak var generalButton: UIButton!
     
     @IBOutlet weak var depressionBtn: UIButton!
     
@@ -32,16 +40,35 @@ class CheckInViewController: UIViewController {
     var stressPick = false;
     var anxietyPick = false;
     var depressionPick = false;
+    var generalPick = false;
+    let resourceView = ResourceViewController()
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        setSleepArray()
+        setAnxietyArray()
+        setAcademicArray()
+        setSressArray()
+        setDepressionArray()
+        setGeneralArray()
+    }
+    
     
     
     @IBAction func sleepClick(_ sender: Any) {
         if(sleepPick == false){
-            sleepBtn.backgroundColor = UIColor.gray;
+            sleepBtn.backgroundColor = UIColor.systemGray5;
+            resourceView.issues.append("sleep");
             sleepPick = true;
             
         }
         else if(sleepPick == true){
             sleepBtn.backgroundColor = UIColor.white;
+            if let index = resourceView.issues.firstIndex(of: "sleep") {
+                resourceView.issues.remove(at: index)
+            }
             sleepPick = false;
         }
     }
@@ -49,12 +76,16 @@ class CheckInViewController: UIViewController {
     
     @IBAction func academicsClick(_ sender: Any) {
         if(academicsPick == false){
-            academicsBtn.backgroundColor = UIColor.gray;
+            academicsBtn.backgroundColor = UIColor.systemGray5;
+            resourceView.issues.append("academics");
             academicsPick = true;
             
         }
         else if(academicsPick == true){
             academicsBtn.backgroundColor = UIColor.white;
+            if let index = resourceView.issues.firstIndex(of: "academics") {
+                resourceView.issues.remove(at: index)
+            }
             academicsPick = false;
         }
     }
@@ -62,60 +93,186 @@ class CheckInViewController: UIViewController {
     
     @IBAction func stressClick(_ sender: Any) {
     if(stressPick == false){
-        stressBtn.backgroundColor = UIColor.gray;
+        stressBtn.backgroundColor = UIColor.systemGray5;
+        resourceView.issues.append("stress");
         stressPick = true;
         
     }
     else if(stressPick == true){
         stressBtn.backgroundColor = UIColor.white;
+        if let index = resourceView.issues.firstIndex(of: "stress") {
+            resourceView.issues.remove(at: index)
+        }
         stressPick = false;
     }
     }
     
     @IBAction func anxietyClick(_ sender: Any) {
         if(anxietyPick == false){
-            anxietyBtn.backgroundColor = UIColor.gray;
+            anxietyBtn.backgroundColor = UIColor.systemGray5;
+            resourceView.issues.append("anxiety");
             anxietyPick = true;
             
         }
         else if(anxietyPick == true){
             anxietyBtn.backgroundColor = UIColor.white;
+            if let index = resourceView.issues.firstIndex(of: "anxiety") {
+                resourceView.issues.remove(at: index)
+            }
             anxietyPick = false;
         }
     }
     
     @IBAction func depressionClick(_ sender: Any) {
         if(depressionPick == false){
-            depressionBtn.backgroundColor = UIColor.gray;
+            depressionBtn.backgroundColor = UIColor.systemGray5;
+            resourceView.issues.append("depression");
             depressionPick = true;
-            
+           
         }
         else if(depressionPick == true){
             depressionBtn.backgroundColor = UIColor.white;
+            if let index = resourceView.issues.firstIndex(of: "depression") {
+                resourceView.issues.remove(at: index)
+            }
             depressionPick = false;
         }
 
     }
  
+    @IBAction func generalClick(_ sender: Any) {
+        if(generalPick == false){
+            generalButton.backgroundColor = UIColor.systemGray5;
+            resourceView.issues.append("general");
+            generalPick = true;
+           
+        }
+        else if(generalPick == true){
+            generalButton.backgroundColor = UIColor.white;
+            if let index = resourceView.issues.firstIndex(of: "general") {
+                resourceView.issues.remove(at: index)
+            }
+            generalPick = false;
+        }
+        
+    }
     @IBAction func submitClick(_ sender: Any) {
-    }
-    
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+   
+    func setSleepArray(){
+        sleep.append(Resource(name: "Sleep Informational Flyer", link: "https://students.wustl.edu/wp-content/uploads/2020/09/Sleep-Flyer-final.pdf", information: "Flyer containing information about healthy sleep"))
+        
+        
     }
-    */
+    func setAnxietyArray(){
+        anxiety.append(Resource(name: "Test & Performance Anxiety", link: "https://students.wustl.edu/wp-content/uploads/2020/09/Test-and-Performance-Anxiety.pdf", information: "Anxiety is NOT the enemy. Moderate levels of anxiety can enhance our performance. However, too much anxiety can interfere with our ability to prepare and perform well."))
+        anxiety.append(Resource(name: "Anxiety Health Information", link: "https://students.wustl.edu/anxiety-health-information/", information: "Health information on anxiety"))
+        anxiety.append(Resource(name: "Counseling", link: "https://students.wustl.edu/counseling/", information: "Information on counseling at Washington University"))
+        anxiety.append(Resource(name: "Mental Health Guidance for Managing Anxiety", link: "https://sites.wustl.edu/takingcare/", information: "Find information, resources, and strategies to help you manage your mental well-being "))
+        }
+    func setAcademicArray(){
+        academics.append(Resource(name: "Procrastination Informational Flyer", link: "https://students.wustl.edu/wp-content/uploads/2020/09/Procrastination.pdf", information: "Procrastination is more about unrealistic expectations,avoidance, coping with stress,or fear of failure."))
+        academics.append(Resource(name: "Perfectionism Informational Flyer", link: "https://students.wustl.edu/wp-content/uploads/2020/09/Perfectionism.pdf", information: "Strategies for Overcoming Perfectionism"))
+        academics.append(Resource(name: "Academic Mentoring", link: "https://learningcenter.wustl.edu/academic-programs/help-with-courses/", information: "Matched mentoring provides supplemental learning."))
+        academics.append(Resource(name: "Different Academic Programs", link: "https://learningcenter.wustl.edu/academic-programs/academic-program-types/", information: "Types of academic programs offered at The Learning Center"))
+            
+    }
+    func setSressArray(){
+        stress.append(Resource(name: "Stress Informational Flyer", link: "https://students.wustl.edu/wp-content/uploads/2020/09/Stress.pdf", information: "Stress is a natural and inevitable part of life. Depending on how we deal with stress, it can help or hinder us. Our goal is to learn how to manage and harness stress."))
+        stress.append(Resource(name: "Deep Breathing Flyer", link: "https://students.wustl.edu/wp-content/uploads/2020/09/Deep-Breathing-Flyer.pdf", information: "TUNE IN.TAKE A STEP BACK.PAY ATTENTION."))
+        stress.append(Resource(name: "Stress Management", link: "https://students.wustl.edu/stress-health-information/", information: "Get tips and resources for managing stress"))
+        
+    }
+    func setDepressionArray(){
+        depression.append(Resource(name: "Mindfulness", link: "https://students.wustl.edu/wp-content/uploads/2020/09/Mindfulness.pdf", information: "PAUSE.NOTICE.REFLECT."))
+        depression.append(Resource(name: "Mental Health Screening", link: "https://screening.mentalhealthscreening.org/wuhealth", information: "Self-evaluation for a depressive disorder"))
+        depression.append(Resource(name: "Depression Health Information", link: "https://students.wustl.edu/depression-health-information/", information: ""))
+        
+            
+    }
+    func setGeneralArray(){
+        general.append(Resource(name: "Mental Health Resources Card", link: "https://students.wustl.edu/wp-content/uploads/2020/09/WashU-Digital-Mental-Health-Resources-Card-to-save-to-your-phone.png", information: ""))
+        general.append(Resource(name: "Mental Health Services Card", link:"https://students.wustl.edu/wp-content/uploads/2020/09/Mental-Health-Services-@-WashU.pdf", information: ""))
+        general.append(Resource(name: "Mental Health Referral Decision Tree", link: "https://students.wustl.edu/wp-content/uploads/2020/11/Mental-Health-Referral-Decision-Tree-1.pdf", information: ""))
+        general.append(Resource(name: "Isolation Wellbeing Plan", link: "https://students.wustl.edu/wp-content/uploads/2020/09/Isolation-Wellbeing-Plan.pdf", information: ""))
+        general.append(Resource(name: "Quarantine Wellbeing Plan", link: "https://students.wustl.edu/wp-content/uploads/2020/09/Quarantine-Wellbeing-Plan.pdf", information: ""))
+        
+    }
+        
+    func setArray(){
+            
+            
+              for x in 0 ..< resourceView.issues.count {
+                   
+                if(resourceView.issues[x] == "sleep"){
+                    for i in 0 ..< sleep.count {
+                        resourceView.resources.append(sleep[i])
+                              
+                    }
+                    double.append(sleep)
+                   
+                }
+                if(resourceView.issues[x] == "academics"){
+                    for i in 0 ..< academics.count {
+                        resourceView.resources.append(academics[i]);
+                    }
+                    double.append(academics)
+                  
+                }
+                if(resourceView.issues[x] == "depression"){
+                    for i in 0 ..< depression.count {
+                        resourceView.resources.append(depression[i]);
+                    }
+                    double.append(depression)
+                }
+                if(resourceView.issues[x] == "anxiety"){
+                     for i in 0 ..< anxiety.count {
+                        resourceView.resources.append(anxiety[i]);
+                    }
+                     double.append(anxiety)
+                }
+                if(resourceView.issues[x] == "stress"){
+                    for i in 0 ..< stress.count {
+                        resourceView.resources.append(stress[i]);
+                    }
+                    double.append(stress)
+                }
+                if(resourceView.issues[x] == "general"){
+                    for i in 0 ..< general.count {
+                        resourceView.resources.append(general[i]);
+                    }
+                    double.append(general)
+                }
+                
+                
+            }
+        
+        }
+
+   
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?)
+    {
+        resourceView.resources.removeAll()
+        double.removeAll()
+        setArray()
+        if let vc = segue.destination as? ResourceListViewController
+        {
+           
+            for i in 0 ..< resourceView.resources.count{
+                
+                vc.resourceArray.append(resourceView.resources[i])
+            }
+            for i in 0 ..< resourceView.issues.count{
+                vc.sections.append(resourceView.issues[i])
+            }
+            
+            vc.doubleResource = double
+            
+        
+        }
+        
+    }
+
 
 }
---
+++ Check-In3.0/Resource.swift
@@ -0,0 +1,23 @@
+//
+//  Resource.swift
+//  Check-In3.0
+//
+//  Created by Oyin Sholeye on 12/10/20.
+//  Copyright © 2020 Check-In. All rights reserved.
+//
+
+import UIKit
+
+struct Resource{
+      
+      var name: String?
+      var link: String?
+      var information: String?
+      
+      init(name: String, link: String, information:String){
+          self.name = name
+          self.link = link
+          self.information = information
+      }
+
+}
